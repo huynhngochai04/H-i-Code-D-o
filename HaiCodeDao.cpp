6056b19c79257275bd388f7ac0b7b34d9c57486b
Huỳnh Ngọc Hải <110411370+huynhngochai04@users.noreply.github.com>
2022-10-17 09:00:19 -0700

    BFS tìm khoảng cách ngắn nhất từ điểm này

--- HaiCodeDao.cpp
+++ HaiCodeDao.cpp
@@ -8,49 +8,62 @@
 #define PI 3.141592653589793238
 using namespace std;
 using ll=long long;
+using db=double;
 inline ll gcd(ll a,ll b){while (a!=b){if (a>b) a-=b; else b-=a;}};
-int main()
+ll cnt=0;
+int n,m,s,t,u,v;
+int a[1001][1001];
+int visited[1001][1001];
+int d[1001][1001];
+int dx[4]={-1,0,0,1};
+int dy[4]={0,-1,1,0};
+void bfs(int i,int j) // tim kiem duong di ngan nhat hang doi
 {
-	string s;
-	getline(cin,s);
-	stringstream ss(s);
-	string tmp="";
-	string pre_check="";
-	int dem=0;
-	stack <int> st;
-	while (ss>>tmp)
+	queue<pair<int,int>> q;
+	q.push({i,j});
+	a[i][j]=1;
+	d[i][j]=0;
+	while(!q.empty())
 	{
-		dem=0;
-		for (char x:tmp)
+		pair<int,int> top=q.front(); q.pop();
+		for (int k=0;k<4;k++)
 		{
-			if (x=='-')
+			int i1=top.first+dx[k];
+			int j1=top.second+dy[k];
+			if (i1>=0&&i1<=n-1&&j1>=0&&j1<=m-1&&a[i1][j1]==a[u][v])
 			{
-				int p=stoi(tmp);
-				st.push(p);
-				break;
-			} else if (x>='0'&&x<='9')
-			{
-				st.push(stoi(tmp));
-				break;
+				d[i1][j1]=d[top.first][top.second]+1;
+				if (i1==u&&j1==v) return;
+				q.push({i1,j1});
+				a[i1][j1]=1;
 			}
 		}
-		if (tmp[0]=='C') st.pop();
-		if (tmp[0]=='D') st.push(st.top()*2);
-		if (tmp[0]=='+') 
+	}
+}
+void inp()
+{
+	cin>>n>>m;
+	for (int i=0;i<n;i++)
+	{
+		for (int j=0;j<m;j++)
 		{
-			int target=st.top();
-			st.pop();
-			int pre_dis=st.top();
-			st.push(target);
-			st.push(target+pre_dis);
+			cin>>a[i][j];
 		}
 	}
-	ll sum=0;
-	while (!st.empty())
+	cin>>s>>t>>u>>v;
+	if (a[s][t]==1||a[u][v]==1||s<0||t<0||u<0||v<0||t>m-1||v>m-1||s>n-1||u>n-1)
 	{
-		sum+=st.top();
-		st.pop();
+		cout<<"-1";
+		return;
 	}
-	cout<<sum;
+	else
+	{
+	bfs(s,t);
+    if (!d[u][v]) cout<<"-1"; else cout<<d[u][v]<<endl;
+	}
+}
+int main()
+{ 
+	inp();
 	return 0;
 }
